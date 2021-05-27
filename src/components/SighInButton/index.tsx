59723d80bdbf17f9bd56240c0fe43190fea4affb
ThiaguinnI <thiaguksianino@gmail.com>
2021-05-27 19:10:18 -0300

    commit inicial

---
+++ src/components/SighInButton/index.tsx
@@ -0,0 +1,24 @@
+import {FaGithub} from 'react-icons/fa'
+import styles from './styles.module.scss'
+import {FiX} from 'react-icons/fi'
+import {signIn,useSession,signOut } from 'next-auth/client'
+
+export function SighInButton(){
+
+const [session] = useSession()
+
+
+
+    return session ? (
+        <button type="button" className={styles.sighInButton} onClick={()=>signOut()}>
+        <FaGithub color="#04d361"/>
+        {session.user.name}
+        <FiX color="#737380" className={styles.closeIcon}/>
+    </button>
+    ) :(
+        <button type="button" className={styles.sighInButton} onClick={()=> signIn('github')}>
+        <FaGithub color="#eba417"/>
+        Sign in with Github
+    </button>
+    )
+}
